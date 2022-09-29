4ac2a490a9bfacdf90e15cd9abd3e8ac2865e6f7
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-29 14:03:56 +0200

    add dummy data and render it

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,11 +1,12 @@
-import { Fragment } from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+	{ id: "q1", author: "Kamil", text: "Learning React is fun" },
+	{ id: "q2", author: "Kamilo", text: "Learning TypeScript is fun" },
+];
 
 const AllQuotes = () => {
-	return (
-		<Fragment>
-			<h1>All Quotes Page</h1>
-		</Fragment>
-	);
+	return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
