8f25cc26502beaf0ec95cbc4e8ae2d430607d7aa
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-09-29 14:16:53 +0200

    add quote details

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -2,14 +2,26 @@ import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+	{ id: "q1", author: "Kamil", text: "Learning React is fun" },
+	{ id: "q2", author: "Kamilo", text: "Learning TypeScript is fun" },
+];
 
 const QuoteDetail = () => {
-	const { quoteId } = useParams();
+	const params = useParams();
+
+	const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+	if (!quote) {
+		return <p>No quote found!</p>;
+	}
 
 	return (
 		<Fragment>
-			<h1>{quoteId}</h1>
-			<Route path={`/quotes/${quoteId}/comments`}>
+			<HighlightedQuote text={quote.text} author={quote.author} />
+			<Route path={`/quotes/${params.quoteId}/comments`}>
 				<Comments />
 			</Route>
 		</Fragment>
