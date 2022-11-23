944dbef18f35ef45f9f9de4fbb5b54f1d9a5116f
MaerS <91330403+KamilKonopski@users.noreply.github.com>
2022-11-24 00:00:42 +0100

    Update MainNavigation.js

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 
-const MainNavigation = () => {
+function MainNavigation() {
 	return (
 		<header className={classes.header}>
 			<div className={classes.logo}>Great Quotes</div>
