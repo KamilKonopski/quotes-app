6bcd9d8975c01e31ed0f2faaa994a86b190dfd45
MaerS <91330403+KamilKonopski@users.noreply.github.com>
2022-11-23 01:12:49 +0100

    Update CommentsList.js

--- src/components/comments/CommentsList.js
+++ src/components/comments/CommentsList.js
@@ -1,7 +1,7 @@
 import CommentItem from './CommentItem';
 import classes from './CommentsList.module.css';
 
-const CommentsList = (props) => {
+function CommentsList(props) {
   return (
     <ul className={classes.comments}>
       {props.comments.map((comment) => (
