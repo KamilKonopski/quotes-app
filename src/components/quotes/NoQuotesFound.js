27d060a4908062d86ae07718b7ea9a01a3c24315
MaerS <91330403+KamilKonopski@users.noreply.github.com>
2022-11-24 00:01:07 +0100

    Update NoQuotesFound.js

--- src/components/quotes/NoQuotesFound.js
+++ src/components/quotes/NoQuotesFound.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 import classes from "./NoQuotesFound.module.css";
 
-const NoQuotesFound = () => {
+function NoQuotesFound() {
 	return (
 		<div className={classes.noquotes}>
 			<p>No quotes found!</p>
